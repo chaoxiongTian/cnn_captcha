1d2470540a47a7ff18953cfc11be1d821cbace2b
chaoxiongtian <953236309@qq.com>
2017-09-18 10:16:31 +0800

    修改裁剪代码

--- sort_file.py
+++ sort_file.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 
-IMAGE_MUMBER = 300
-IMAGE_PATH = "/home/tianchaoxiong/LinuxData/data/verifies/full_experiment/baidu_image_captcha/"
+IMAGE_MUMBER_ALL = 34 #一共有多少个图片
+IMAGE_MUMBER_CPOE = 5 #需要拷贝多少图片
+IMAGE_PATH = "/home/tianchaoxiong/LinuxData/data/verifies/full_experiment/error_image/"
 LABEL_PATH = "/home/tianchaoxiong/LinuxData/data/verifies/full_experiment/"
-LABEL_FILE_NAME = "baidu_labels_captcha.txt"
+LABEL_FILE_NAME = "error_labels_text.txt"
 COPE_PATH = "/home/tianchaoxiong/LinuxData/data/verifies/full_experiment/dev/"
 if not os.path.exists(COPE_PATH):
     os.makedirs(COPE_PATH)
@@ -12,18 +13,21 @@ IMAGE_FORMAT = ".jpg"
 
 
 def getStrContent(path):
-    return open(path, 'r').read()
+    return open(path, 'r').read().strip()
 
 
 def save_image(image_list):
     for each in range(len(image_list)):
-        path1 = image_list[each]
+        path1 = image_list[each]+ IMAGE_FORMAT
         path2 = COPE_PATH + str(each) + IMAGE_FORMAT
+        print(path1)
+        print(path2)
         cope_image(path1, path2)
 
 
 def cope_image(file_path_original, file_path_target):
-    shutil.copy(file_path_original, file_path_target)
+    # shutil.cope(file_path_original, file_path_target)
+    shutil.move(file_path_original, file_path_target)
 
 
 def save_labels(labels_list):
@@ -44,11 +48,11 @@ def main():
     label_path = LABEL_PATH
     label_path = label_path + LABEL_FILE_NAME
     images_path = []
-    for each in range(IMAGE_MUMBER):
-        images_path.append(image_path + str(each) + IMAGE_FORMAT)
+    for each in range(IMAGE_MUMBER_ALL):
+        images_path.append(image_path + str(each))
     string = getStrContent(label_path)
     labels = string.split("#")
-    cope_file(images_path[-IMAGE_MUMBER:], labels[-IMAGE_MUMBER:])
+    cope_file(images_path[-IMAGE_MUMBER_CPOE:], labels[-IMAGE_MUMBER_CPOE:])
 
 
 if __name__ == '__main__':
